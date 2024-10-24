5c31c2c868a9241700223cb124d5e4dacac972fc
ArtemChip <plentypvp@gmail.com>
2024-10-24 17:31:03 +0500

    Мое местоположение и карта

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.pm.binomtech_test"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pm.binomtech_test"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.activity:activity-compose:1.9.2")
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/pm/binomtech_test/MainActivity.kt
+++ app/src/main/java/com/pm/binomtech_test/MainActivity.kt
@@ -1,46 +1,144 @@
 package com.pm.binomtech_test
 
-import android.os.Bundle
+import android.Manifest
+import android.content.pm.*
+import android.location.*
+import android.os.*
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.pm.binomtech_test.ui.theme.Binomtech_testTheme
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationRequest
+import org.osmdroid.config.Configuration
+import org.osmdroid.events.*
+import org.osmdroid.tileprovider.tilesource.*
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.*
+import org.osmdroid.views.overlay.mylocation.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), IMyLocationProvider {
+    private var mapView: MapView? = null
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var locationRequest: LocationRequest? = null
+    private var locationCallback: LocationCallback? = null
+    private var lastLocation: Location? = null
+    private var locationRunning = false
+    private var locConsumer: IMyLocationConsumer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Binomtech_testTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+        locationRunning = false
+        setupMap()
+        checkPermissionsState()
+    }
+
+    private fun checkPermissionsState() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            setupCurrentLocation()
+        } else {
+            val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { grantResults: Map<String, Boolean> ->
+                if (grantResults.all { it.value }) {
+                    setupCurrentLocation()
+                } else {
+                    Toast.makeText(this, R.string.loc_permission_required, Toast.LENGTH_LONG).show()
+                }
+            }
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_WIFI_STATE
+                )
+            )
+        }
+    }
+
+    private fun setupMap() {
+        mapView = findViewById(R.id.mapview)
+        mapView!!.isClickable = true
+        mapView!!.controller.setZoom(15.0)
+        mapView!!.controller.setCenter(GeoPoint(54.733334, 56.0))
+        mapView!!.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        Configuration.getInstance().userAgentValue = "Chrome"
+        mapView!!.setMapListener(DelayedMapListener(object : MapListener {
+            override fun onZoom(e: ZoomEvent): Boolean {
+                return true
+            }
+
+            override fun onScroll(e: ScrollEvent): Boolean {
+                return true
+            }
+        }, 100))
+    }
+
+    private fun setupCurrentLocation() {
+        // Launch Process
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateIntervalMillis(500)
+            .setMaxUpdateDelayMillis(1000)
+            .build()
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult.locations.isNotEmpty()) {
+                    lastLocation = locationResult.lastLocation
+                    locConsumer?.onLocationChanged(lastLocation, this@MainActivity)
                 }
             }
         }
-    }
-}
+        fusedLocationClient?.requestLocationUpdates(
+            locationRequest!!,
+            locationCallback!!,
+            null
+        )
+        locationRunning = true
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Binomtech_testTheme {
-        Greeting("Android")
+        // Marker
+        val oMapLocationOverlay = MyLocationNewOverlay(this, mapView)
+        oMapLocationOverlay.enableFollowLocation()
+        oMapLocationOverlay.enableMyLocation()
+        mapView!!.overlays.add(oMapLocationOverlay)
     }
+
+    override fun onPause() {
+        super.onPause()
+        if (locationRunning) {
+            fusedLocationClient?.removeLocationUpdates(locationCallback!!)
+            locationRunning = false
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!locationRunning && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient?.requestLocationUpdates(
+                locationRequest!!,
+                locationCallback!!,
+                null
+            )
+            locationRunning = true
+        }
+    }
+
+    override fun startLocationProvider(myLocationConsumer: IMyLocationConsumer?): Boolean {
+        locConsumer = myLocationConsumer
+        return true
+    }
+
+    override fun stopLocationProvider() {
+        locConsumer = null
+    }
+
+    override fun getLastKnownLocation(): Location {
+        return lastLocation ?: Location("")
+    }
+
+    override fun destroy() {}
 }
